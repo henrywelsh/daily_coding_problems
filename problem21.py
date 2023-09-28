739709cf0314ee4447c3a07f6dee2dc508ae5710
henry-25 <henry.r.welsh@gmail.com>
2023-09-28 10:55:07 -0700

    Problem 21 solution

---
+++ problem21.py
@@ -0,0 +1,26 @@
+# Given an array of time intervals (start, end) for classroom lectures (possibly overlapping),
+# find the minimum number of rooms required.
+#
+# For example, given [(30, 75), (0, 50), (60, 150)],
+# you should return 2.
+
+def find_min_rooms(time_intervals):
+    num_overlaps = [0 for i in range(len(time_intervals))]
+    for i in range(0, len(time_intervals)):
+        for j in range(i, len(time_intervals)):
+            if check_overlap(time_intervals[i], time_intervals[j]):
+                num_overlaps[i] += 1
+                num_overlaps[j] += 1
+    return max(num_overlaps)
+
+
+def check_overlap(ti1, ti2):
+    first_interval_start = ti1[0]
+    first_interval_stop = ti1[1]
+    second_interval_start = ti2[0]
+    second_interval_stop = ti2[1]
+    return first_interval_start < second_interval_start < first_interval_stop or second_interval_start < first_interval_start < second_interval_stop
+
+
+if __name__ == "__main__":
+    print(find_min_rooms([(30, 75), (0, 50), (60, 150)]))
